c30901ddc91e26fb915049285a2d31befbac72ee
AvaKum <avaneesh.paper@gmail.com>
2017-04-28 15:02:32 +0530

    Update L1_ load and display image.m

--- 2A/L1_Images-Functions/L1_ load and display image.m
+++ 2A/L1_Images-Functions/L1_ load and display image.m
@@ -1,1 +1,24 @@
-% Load and display an imageimg = imread('dolphin.png');imshow(tablet);%Image size disp(size(img));% Image Class or data type disp(class(img));%--------------k = imread('IMG_5456.JPG');L = imread('IMG_5459.JPG');disp(img)diff = k - L ; imshow(diff) ; 
+
+% Load and display an image
+
+img = imread('dolphin.png');
+imshow(tablet);
+
+%Image size 
+disp(size(img));
+
+% Image Class or data type 
+disp(class(img));
+
+
+%--------------
+% IMAGE DIFFERENCE
+
+k = imread('IMG_5456.JPG');
+L = imread('IMG_5459.JPG');
+disp(img)
+
+diff = k - L ; 
+imshow(diff) ; 
+
+
